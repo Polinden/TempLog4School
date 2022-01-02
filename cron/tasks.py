9c73dade3a560b50043e7b37b166eb46b23bd085
Den Wolper <denwolper@MacBook-Pro-Den.local>
2022-01-02 21:07:47 +0200

    added city and users table

--- cron/tasks.py
+++ cron/tasks.py
@@ -16,20 +16,22 @@ APIWET=os.environ['APIWET']
 def w_task(self):
     con=0
     try:
+        conn = psycopg2.connect(dbname='postgres', user='postgres', password='leedan', host='172.17.0.3') 
+       
         s=requests.get(APIWET)
         if s.ok:
             s=json.loads(s.text)
             s=s['main']['temp']
         else:
-            s=''
-        conn = psycopg2.connect(dbname='postgres', user='postgres', 
-                        password='leedan', host='172.17.0.3') 
-        sql = ''' INSERT INTO project1_weather(updated, temp)
-              VALUES(%s,%s) '''
+            s='Error'
+
+        sql = ''' INSERT INTO project1_weather(updated, temp, city_id) VALUES(%s,%s,%s) '''
+
         cur = conn.cursor()
-        cur.execute(sql, (datetime.datetime.now(tz=pytz.timezone('EUROPE/KIEV')),float(s)))
+        cur.execute(sql, (datetime.datetime.now(tz=pytz.timezone('EUROPE/KIEV')),float(s),1))  #todo!
         conn.commit()
         cur.close()
+
     except Exception as e:
         print(e)
     finally:
