3b0d17a6acccedf6b6d78355f005af1979f6b7df
Den Wolper <denwolper@MacBook-Pro-Den.local>
2022-01-05 14:47:19 +0200

    bug fixed in args of func get_conn

--- code/project1/views.py
+++ code/project1/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Weather, City
 from django.db.models import Avg, F, TimeField
 from django.db.models.functions import TruncDate, TruncHour
-from django.views.decorators.cache import cache_page
+from django.views.decorators.cache import cache_page, vary_on_cookie
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.http import HttpResponseRedirect
 from django.utils import timezone
