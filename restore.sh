d9e67666ae9699ec00b1e0bdd0675812416aba61
wolper <denwolper@MacBook-Pro-Den.local>
2022-01-11 08:09:44 +0200

    bug with moment and Date fixed

--- restore.sh
+++ restore.sh
@@ -1,5 +1,5 @@
   docker cp ~/4dump/temp.dm postgres:vol_db/
-  docker exec -i postgres psql -U postgres -c "insert into project1_city (name) values ('Alicante');"
+  #docker exec -i postgres psql -U postgres -c "insert into project1_city (name) values ('Alicante');"
   docker exec -i postgres psql -U postgres -c "delete from project1_weather;"
   docker exec -i postgres psql -U postgres -c "copy project1_weather from '/vol_db/temp.dm' csv header;"
   docker exec -i postgres psql -U postgres -c "SELECT setval('project1_weather_id_seq', (SELECT MAX(id) FROM project1_weather));"
